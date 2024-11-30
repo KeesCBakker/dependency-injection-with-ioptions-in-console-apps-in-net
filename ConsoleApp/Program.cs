41d18912f77e03b9f2de8f4fee5132bd0c3dd01a
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2024-11-30 08:19:36 +0100

    Upgrade to .NET 8
    Support for IOption data validation validation on startup

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System.CommandLine;
 
 static void ConfigureServices(IServiceCollection services)
@@ -19,7 +20,17 @@ static void ConfigureServices(IServiceCollection services)
         .AddEnvironmentVariables()
         .Build();
 
-    services.Configure<AppSettings>(configuration.GetSection("App"));
+    void Configure<TConfig>(string sectionName) where TConfig : class
+    {
+        services
+            .AddSingleton(p => p.GetRequiredService<IOptions<TConfig>>().Value)
+            .AddOptions<TConfig>()
+            .Bind(configuration.GetSection(sectionName))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+    }
+
+    Configure<AppOptions>(AppOptions.SectionName);
 
     // add services:
     // services.AddTransient<IMyRespository, MyConcreteRepository>();
@@ -36,4 +47,4 @@ ConfigureServices(services);
 using var serviceProvider = services.BuildServiceProvider();
 
 // entry to run app
-await serviceProvider.GetService<App>().InvokeAsync(args);
+await serviceProvider.GetRequiredService<App>().InvokeAsync(args);
--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# Dependency injection (with IOptions) in Console Apps in .NET 7
+# Dependency injection (with IOptions) in Console Apps in .NET 8
 
 This project contains the code of the article <a href="https://keestalkstech.com/2018/04/dependency-injection-with-ioptions-in-console-apps-in-net-core-2/">Dependency injection (with IOptions) in Console Apps in .NET</a> on my blog.
 
 ## Features
 
-- Support for .NET 7
+- Support for .NET 8
 - Support for <a href="https://learn.microsoft.com/en-us/dotnet/api/system.commandline.rootcommand">System.CommandLine.RootCommand</a>
 - Support for appsettings.json configuration
 - Support for environment variables configuration
 - Support for IOption injection
+- Support for IOption data validation validation on startup
